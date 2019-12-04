cf85865d4ae2a16146308101640f526dedb54e83
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-04 20:46:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,48 @@
-# My Code here....
+##########################
+##### MAP-Y METHODS ######
+##########################
+
+def map_to_negativize(source_array)
+  negativized_array = []
+  for item in source_array do
+    negativized_array << (item * -1)
+  end
+  negativized_array
+end
+
+def map_to_no_change(source_array)
+  source_array
+end
+
+def map_to_double(source_array)
+  double_array = []
+  for item in source_array do
+    double_array << (item * 2)
+  end
+  double_array
+end
+
+def map_to_square(source_array)
+  square_array = []
+  for item in source_array do
+    square_array << (item ** 2)
+  end
+  square_array
+end
+
+
+##########################
+#### REDUCE-Y METHODS ####
+##########################
+
+def reduce_to_total(source_array, starting_point)
+  
+end
+
+def reduce_all_to_true(source_array)
+  
+end
+
+def reduce_any_to_true(source_array)
+  
+end
